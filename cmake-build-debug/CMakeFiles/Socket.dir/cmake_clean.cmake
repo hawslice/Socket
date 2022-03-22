b3751941b96e296ab57f1e5bfe01916f8319070e
Teow <1505692103@qq.com>
2022-03-22 15:59:05 +0800

    origin

---
+++ cmake-build-debug/CMakeFiles/Socket.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Socket.dir/main.cpp.obj"
+  "Socket.exe"
+  "Socket.exe.manifest"
+  "Socket.pdb"
+  "libSocket.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
